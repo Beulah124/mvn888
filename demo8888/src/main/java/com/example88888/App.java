ff2c168dca4d5722666c2847433943b29d63bed0
Beulah124 <sekhar.cse@cmrtc.ac.in>
2026-09-24 10:59:37 +0530

    mvn project

---
+++ demo8888/src/main/java/com/example88888/App.java
@@ -0,0 +1,13 @@
+package com.example88888;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
